0545a6b030b5061eccd5513659a74c824316e5db
JhayCodes <jhayycodes@gmail.com>
2023-09-14 08:16:25 +0000

    first commit

---
+++ Auction-Web-App/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Auction_Web_App.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+
+        
+    }
+}
---
+++ Auction-Web-App/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Auction_Web_App.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        //fk for listing 
+        public int listingId { get; set; }
+        public Listing listing { get; set; }
+
+        [Required]
+        public string? userId { get; set; }
+        [ForeignKey("userId")]
+        public ApplicationUser? user { get; set; }
+
+    }
+}
